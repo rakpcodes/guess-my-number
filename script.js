06545eabc8b0d2fe8560d256b2dfb63b79ae421a
rakpcodes <rakpcodes@gmail.com>
2022-09-29 18:51:32 -0400

    added event listener, check for empty guess

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+
+    const guess = Number(document.querySelector('.guess').value)
+
+    if (!guess) {
+        document.querySelector('.message').textContent = `You didn't pick anything!🥺 Pick a number!`
+    }
+});
