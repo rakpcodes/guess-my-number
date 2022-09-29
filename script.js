6829d12fc1fdb2cf285168c744a6e24abab17129
rakpcodes <rakpcodes@gmail.com>
2022-09-29 19:26:08 -0400

    added game logic, checks score against random number, updates message and score

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 'use strict';
 
+const secretNumber = Math.floor(Math.random() * 20) + 1
+let score = 20
+
 document.querySelector('.check').addEventListener('click', function () {
 
     const guess = Number(document.querySelector('.guess').value)
 
     if (!guess) {
         document.querySelector('.message').textContent = `You didn't pick anything!🥺 Pick a number!`
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = `Correct! 🎉`
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = `Too High! Try lower? 🔽`;
+        score--
+        document.querySelector('.score').textContent = score
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = `Too Low! Try higher? 🔼`
+        score--
+        document.querySelector('.score').textContent = score
     }
 });
