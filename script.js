8432089dca231e9a4fd7e09a91a445f7ae9d3c54
rakpcodes <rakpcodes@gmail.com>
2022-09-29 22:02:37 -0400

    Coding challenge complete. Again button resets interface to try again. added logic to update high score

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 20) + 1
+let secretNumber = Math.floor(Math.random() * 20) + 1
 let score = 20
+console.log(secretNumber)
 
 document.querySelector('.check').addEventListener('click', function () {
 
     const guess = Number(document.querySelector('.guess').value)
+    const highScore = Number(document.querySelector('.highscore').innerText)
 
     if (!guess) {
         document.querySelector('.message').textContent = `You didn't pick anything!🥺 Pick a number!`
@@ -15,6 +17,9 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.number').style.width = '30rem'
         document.querySelector('body').style.backgroundColor = '#60b347'
 
+        if (score > highScore) {
+            document.querySelector('.highscore').innerText = score
+        }
 
 
     } else if (guess > secretNumber) {
@@ -34,3 +39,31 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.score').textContent = score
     }
 });
+
+// CODING CHALLENGE
+
+/* 
+
+Implement a game reset functionality so that the player can make a new guess using the 'Again!' button.
+
+
+Notes:
+
+- Re-randomize secretNumber
+- Reset score and display to .score
+- Reset .message
+- Clear .guess so past guess doesnt remain
+- Clear victory style manipulation
+
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+    secretNumber = Math.floor(Math.random() * 20) + 1
+    score = 20
+    document.querySelector('.score').textContent = score
+    document.querySelector('.message').textContent = `Start guessing...`
+    document.querySelector('.guess').value = ''
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').textContent = '?'
+})
